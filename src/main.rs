feb17f18a30be69dc7c600817f29278eda62c634
VishnuMukund24 <vishnumukundar@gmail.com>
2025-07-03 07:00:55 +0530

    Added tracker crate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+mod tracker;
 mod replayer;
+
 
 use rdev::{listen, Event, EventType, Button, Key};
 use std::{process::exit, sync::{Arc, Mutex}};
 use std::time::Instant;
 use serde::{Serialize, Deserialize};
-use std::fs::File;
-use std::io::{Write, BufWriter};
 
+use crate::tracker::Tracker;    // <--- Tracker struct
 use crate::replayer::Replayer;  // <--- Replayer struct
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -17,43 +18,6 @@ pub struct MouseEvent {
     pub time: f64,
 }
 
-// Tracker struct
-pub struct Tracker {
-    pub events: Vec<MouseEvent>,
-}
-
-impl Tracker {
-    pub fn new() -> Self {
-        Tracker {
-            events: Vec::new(),
-        }
-    }
-
-    pub fn add_event(&mut self, x: i32, y: i32, button: Option<String>, time: f64) {
-        self.events.push(MouseEvent { x, y, button, time });
-    }
-
-    pub fn save_as_csv(&self, filename: &str) {
-        let file = File::create(filename).expect("Could not create CSV file");
-        let mut writer = BufWriter::new(file);
-
-        writeln!(writer, "x,y,button,time").expect("Could not write header");
-
-        for event in &self.events {
-            let button = event.button.clone().unwrap_or_else(|| "None".to_string());
-            writeln!(writer, "{},{},{},{}", event.x, event.y, button, event.time).expect("Could not write row");
-        }
-
-        println!("Events saved to {}", filename);
-    }
-
-    pub fn save_as_json(&self, filename: &str) {
-        let file = File::create(filename).expect("Could not create JSON file");
-        serde_json::to_writer_pretty(file, &self.events).expect("Could not write JSON");
-
-        println!("Events saved to {}", filename);
-    }
-}
 
 fn main() {
     let start = Instant::now();
